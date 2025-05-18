e57ff50954f66e3f95b6880d985f3c70645e18d3
RyanDusty <ryan.tapfield@gmail.com>
2025-05-18 23:12:08 +1000

    Fixed every single issue (im da goat)
    Just needed to format n stuff

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,70 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         CSV csv = new CSV();
-
-        csv.createCSVFile(BankAccount.filepath);
-
         BankAccount instance = new BankAccount(0, "", 0);
+        if(BankAccount.filepath == null) {
+            System.out.println("File path is not set, creating a new file.");
+            csv.createCSVFile(BankAccount.filepath);
+            Scanner scanner = new Scanner(System.in);
 
-        csv.saveToCSV(instance, BankAccount.filepath);
+            System.out.println("Enter Account Number: ");
+            int accountNumber = scanner.nextInt();
+            instance.setAccountNumber(accountNumber);
 
-        csv.loadFromCSV(BankAccount.filepath);
+            System.out.println("Enter Account Name: ");
+            String accountName = scanner.next();
+            instance.setAccountName(accountName);
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter Account Number: ");
-        int accountNumber = scanner.nextInt();
+            System.out.println("Enter Account Balance: ");
+            float accountBalance = scanner.nextFloat();
+            instance.setAccountBalance(accountBalance);
 
-        System.out.println("Enter Account Name: ");
-        String accountName = scanner.next();
+            System.out.println("Updated Account:");
+            System.out.println("Number: " + instance.getAccountNumber());
+            System.out.println("Name: " + instance.getAccountName());
+            System.out.println("Balance: " + instance.getAccountBalance());
+            csv.saveToCSV(instance, BankAccount.filepath);
+            System.out.println("Account saved to CSV file.");
+            scanner.close();
+        }
+        else {
+  
+            System.out.println("File path is set.");
+            csv.loadFromCSV(BankAccount.filepath);
+            System.out.println("Account loaded from CSV file.");
 
-        System.out.println("Enter Account Balance: ");
-        float accountBalance = scanner.nextFloat();
 
-        System.out.println("Updated Account:");
-        System.out.println("Number: " + instance.getAccountNumber());
-        System.out.println("Name: " + instance.getAccountName());
-        System.out.println("Balance: " + instance.getAccountBalance());
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Do you want to update the account? (yes/no)");
+            String yesno = scanner.next();  
+            if (yesno.equalsIgnoreCase("yes")) {
+                System.out.println("Enter new Account Number: ");
+                int accountNumber = scanner.nextInt();
+                instance.setAccountNumber(accountNumber);
+
+                System.out.println("Enter new Account Name: ");
+                String accountName = scanner.next();
+                instance.setAccountName(accountName);
+
+                System.out.println("Enter new Account Balance: ");
+                float accountBalance = scanner.nextFloat();
+                instance.setAccountBalance(accountBalance); 
+
+                System.out.println("Updated Account:");
+                System.out.println("Number: " + instance.getAccountNumber()); 
+                System.out.println("Name: " + instance.getAccountName());
+                System.out.println("Balance: " + instance.getAccountBalance());
+                csv.saveToCSV(instance, BankAccount.filepath);
+
+                scanner.close();
+            }
+        }
+
+        
+
+
+        
 
     }
 }
