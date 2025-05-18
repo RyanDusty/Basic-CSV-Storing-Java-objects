62f011cd97f281ffcf1873055fb494f96bac2311
RyanDusty <ryan.tapfield@gmail.com>
2025-05-18 22:01:42 +1000

    Pretty much finished everything.
    Some files are just broken though. Values keep resetting everytime I re-run the jar, the instance isn't working in the main class. Good learning experience though.

---
+++ src/CSV.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class CSV {
+    public void saveToCSV(BankAccount account, String filepath) {
+            try (FileWriter writer = new FileWriter(filepath, true)) {
+                writer.write(
+                        account.accountNumber + "," +
+                                account.accountName + "," +
+                                account.accountBalance + "\n"
+                );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    public void loadFromCSV(String filepath) {
+    File file = new File(filepath);
+    try (Scanner scanner = new Scanner(file)) {
+        boolean isFirstLine = true; // Flag to skip header
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            // Skip the header line
+            if (isFirstLine) {
+                isFirstLine = false;
+                continue; // Move to the next iteration
+            }
+
+            String[] data = line.split(",");
+            int accountNumber = Integer.parseInt(data[0]);
+            String accountName = data[1];
+            float accountBalance = Float.parseFloat(data[2]);
+
+            // Print or process the data
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Account Name: " + accountName);
+            System.out.println("Balance: " + accountBalance);
+        }
+    } catch (FileNotFoundException e) {
+        throw new RuntimeException(e);
+    }
+}
+
+    
+    public void createCSVFile(String filepath) {
+        File file = new File(filepath);
+        if (!file.exists()) {
+            try (FileWriter writer = new FileWriter(filepath)) {
+                writer.write("Account Number,Account Name,Account Balance\n");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,32 @@
-import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    private static final String CSV_FILE = "account.csv";
     public static void main(String[] args) {
-        // File creation
-        File file = new File(CSV_FILE);
-        if(file.exists()) {
+        CSV csv = new CSV();
 
-        }
-        // Instance of class objectClass
-        System.out.println("Account Details:");
-            System.out.println("What's your balance?");
-        Scanner Balance = new Scanner(System.in);
-            float Bal = Balance.nextFloat();
-        BankAccount instance = BankAccount.EMPTY;
-            instance.setAccountBalance(Bal);
-        Float balance = instance.getAccountBalance();
-            System.out.println(Bal);
-        // Account number system
-        System.out.println("What's your account number?");
-            Scanner num = new Scanner(System.in);
-        int number = num.nextInt();
-            instance.setAccountNumber(number);
-        int accountnum = instance.getAccountNumber();
-            System.out.println(accountnum);
-        // Account name system
-        System.out.println("What's your accounts name?");
-            Scanner name = new Scanner(System.in);
-        String nam = name.next();
-            instance.setAccountName(nam);
-        String accountname1 = instance.getAccountName();
-            System.out.println(accountname1);
+        csv.createCSVFile(BankAccount.filepath);
+
+        BankAccount instance = new BankAccount(0, "", 0);
+
+        csv.saveToCSV(instance, BankAccount.filepath);
+
+        csv.loadFromCSV(BankAccount.filepath);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Account Number: ");
+        int accountNumber = scanner.nextInt();
+
+        System.out.println("Enter Account Name: ");
+        String accountName = scanner.next();
+
+        System.out.println("Enter Account Balance: ");
+        float accountBalance = scanner.nextFloat();
+
+        System.out.println("Updated Account:");
+        System.out.println("Number: " + instance.getAccountNumber());
+        System.out.println("Name: " + instance.getAccountName());
+        System.out.println("Balance: " + instance.getAccountBalance());
 
     }
 }
